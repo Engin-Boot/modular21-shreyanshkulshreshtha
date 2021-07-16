f3beb700a85cb75b7af02427686a27696064a4cc
Shreyansh Kulshreshtha <shreyanshkuls@gmail.com>
2021-07-16 11:39:38 +0530

    Optimize imports

--- TelCo/ColorCoder.cs
+++ TelCo/ColorCoder.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Text;
-using TelCo.TestWireColoringSystem;
 
 namespace TelCo.WireColoringSystem
 {
--- TelCo/TestReferenceManual.cs
+++ TelCo/TestReferenceManual.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Diagnostics;
 using TelCo.WireColoringSystem;
 
